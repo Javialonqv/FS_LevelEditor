90b9a7d3b3e8161ccf8bba2613da89fd9439c478
Javier Quintero <alonsoquinterovizcarra@gmail.com>
2024-12-23 17:41:17 -0700

    Initial commit.
    Only added a base and a camera movement (no rotation).

---
+++ Core.cs
@@ -0,0 +1,87 @@
+﻿using MelonLoader;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[assembly: MelonInfo(typeof(FS_LevelEditor.Core), "FS_LevelEditor", "1.0.0", "Javier", null)]
+[assembly: MelonGame("Haze Games", "Fractal Space")]
+
+namespace FS_LevelEditor
+{
+    public class Core : MelonMod
+    {
+        GameObject groundObj;
+
+        static readonly Vector3 groundBaseTopLeftPivot = new Vector3(-17f, 121f, -72f);
+
+        public override void OnInitializeMelon()
+        {
+            LoadAssetBundle();
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+#if DEBUG
+            if (sceneName.Contains("Menu"))
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.parent = Camera.main.transform;
+                cube.transform.localPosition = Vector3.zero;
+                cube.transform.rotation = Quaternion.identity;
+            }
+#endif
+        }
+
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.Keypad5))
+            {
+                SetupEditorUI();
+                SpawnBase();
+            }
+        }
+
+        void SetupEditorUI()
+        {
+            GameObject.Find("MainMenu/Camera/Holder/Main").SetActive(false);
+            GameObject.Find("MainMenu/Camera/Holder/Navigation").SetActive(false);
+
+            GameObject.Find("Level").SetActive(false);
+
+            GameObject.Destroy(GameObject.Find("Main Camera").GetComponent<Animation>());
+            GameObject.Find("Main Camera").transform.position = new Vector3(-15f, 125f, -75f);
+            GameObject.Find("Main Camera").transform.localEulerAngles = new Vector3(45f, 0f, 0f);
+
+            GameObject.Find("Main Camera").AddComponent<EditorCameraMovement>();
+        }
+
+        void SpawnBase()
+        {
+            for (int width = 0; width < 3; width++)
+            {
+                for (int height = 0; height < 3; height++)
+                {
+                    Vector3 position = groundBaseTopLeftPivot;
+                    position.x += width * 2f;
+                    position.z += height * 2f;
+
+                    GameObject.Instantiate(groundObj).transform.position = position;
+                }
+            }
+        }
+
+        void LoadAssetBundle()
+        {
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("FS_LevelEditor.level_editor");
+            byte[] bytes = new byte[stream.Length];
+            stream.Read(bytes);
+
+            Il2CppAssetBundle bundle = Il2CppAssetBundleManager.LoadFromMemory(bytes);
+
+            groundObj = bundle.Load<GameObject>("Ground");
+            groundObj.hideFlags = HideFlags.DontUnloadUnusedAsset;
+
+            bundle.Unload(false);
+        }
+    }
+}
