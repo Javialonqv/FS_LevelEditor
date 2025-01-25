2451326bd57da6886d135fab5ce54db1a7725522
Javier Quintero <alonsoquinterovizcarra@gmail.com>
2025-01-25 12:37:03 -0700

    Fixed mod version string.

--- Core.cs
+++ Core.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-[assembly: MelonInfo(typeof(FS_LevelEditor.Core), "FS_LevelEditor", "v0.1.0", "Javialon_qv", null)]
+[assembly: MelonInfo(typeof(FS_LevelEditor.Core), "FS_LevelEditor", "0.1.0", "Javialon_qv", null)]
 [assembly: MelonGame("Haze Games", "Fractal Space")]
 
 namespace FS_LevelEditor
