b84e529112eb45e521bce330882a2ec4f050aca7
Javier Quintero <alonsoquinterovizcarra@gmail.com>
2025-01-24 17:57:21 -0700

    Added obj limit for directional lights (limit is 1, lmao).

--- Specific_Objects_Scripts/LE_Directional_Light.cs
+++ Specific_Objects_Scripts/LE_Directional_Light.cs
@@ -10,16 +10,32 @@ namespace FS_LevelEditor
     [MelonLoader.RegisterTypeInIl2Cpp]
     public class LE_Directional_Light : LE_Object
     {
+        // Since there's only ONE directional light per level, dont' even put the id on it.
+        public override string objectFullNameWithID
+        {
+            get { return objectOriginalName; }
+        }
+
         GameObject lightSprite;
+
+        static int currentInstances = 0;
+        const int maxInstances = 1;
 
         void Awake()
         {
+            currentInstances++;
+
             lightSprite = gameObject.GetChildWithName("Sprite");
         }
 
         void Update()
         {
             lightSprite.transform.rotation = Camera.main.transform.rotation;
+        }
+
+        void OnDestroy()
+        {
+            currentInstances--;
         }
     }
 }
