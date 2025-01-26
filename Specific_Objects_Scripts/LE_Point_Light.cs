0e8d7998c85a384fa117da99e9db479115b8199c
Javier Quintero <alonsoquinterovizcarra@gmail.com>
2025-01-25 22:23:21 -0700

    Added point lights.

---
+++ Specific_Objects_Scripts/LE_Point_Light.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FS_LevelEditor
+{
+    [MelonLoader.RegisterTypeInIl2Cpp]
+    public class LE_Point_Light : LE_Object
+    {
+        GameObject lightBulbSprite;
+
+        void Awake()
+        {
+            lightBulbSprite = gameObject.GetChildWithName("Sprite");
+        }
+
+        void Update()
+        {
+            lightBulbSprite.transform.rotation = Camera.main.transform.rotation;
+        }
+    }
+}
