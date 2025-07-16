ea49d7d5abdb3c6a83824dcbc382da96a8e06392
Javier Quintero <alonsoquinterovizcarra@gmail.com>
2025-07-15 22:07:01 -0700

    Now every text that says "Chapter 4" is patched.

---
+++ Playmode/Patches/ChapterLocPatch.cs
@@ -0,0 +1,24 @@
+﻿using Il2Cpp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FS_LevelEditor.Playmode.Patches
+{
+    [HarmonyLib.HarmonyPatch(typeof(Localization), nameof(Localization.Get))]
+    public static class ChapterLocPatch
+    {
+        public static bool Prefix(ref string __result, string key)
+        {
+            if (key == "Chapter4" && PlayModeController.Instance)
+            {
+                __result = PlayModeController.Instance.levelName;
+                return false; // Skip the original method
+            }
+
+            return true;
+        }
+    }
+}
