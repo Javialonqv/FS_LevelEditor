64f0f063fd8c840cf7075bb230f36dfc0e46fc7f
Javier Quintero <alonsoquinterovizcarra@gmail.com>
2025-07-23 21:54:41 -0700

    Organized PlayModeController.cs methods.

--- Playmode/PlayModeController.cs
+++ Playmode/PlayModeController.cs
@@ -31,9 +31,10 @@ namespace FS_LevelEditor.Playmode
         GameObject[] otherObjectsFromBundle;
         public GameObject levelObjectsParent;
 
+        public Dictionary<string, object> globalProperties = LevelData.GetDefaultGlobalProperties();
+
         GameObject backToLEButton;
 
-        public Dictionary<string, object> globalProperties = LevelData.GetDefaultGlobalProperties();
         public List<LE_Object> currentInstantiatedObjects = new List<LE_Object>();
         public int deathsInCurrentLevel = 0;
         public List<LE_Screen> screensOnTheLevel = new List<LE_Screen>();
@@ -46,172 +47,14 @@ namespace FS_LevelEditor.Playmode
             Instance = this;
 
             LoadAssetBundle();
+            levelObjectsParent = new GameObject("LevelObjects");
+            levelObjectsParent.transform.position = Vector3.zero;
+            CreateBackToLEButton();
+            PlaymodePauseMenuPatcher.Create();
 
             deathsInCurrentLevel = Melon<Core>.Instance.totalDeathsInCurrentPlaymodeSession;
 
             Invoke("DisableTheCurrentScene", 0.2f);
-
-            levelObjectsParent = new GameObject("LevelObjects");
-            levelObjectsParent.transform.position = Vector3.zero;
-
-            CreateBackToLEButton();
-
-            PlaymodePauseMenuPatcher.Create();
-        }
-
-        void Start()
-        {
-            TeleportPlayer();
-            ConfigureGlobalProperties();
-
-            UnloadBundle();
-        }
-
-        // When the script obj is destroyed, that means the scene has changed, destroy the back to LE button, since it'll be created again when entering...
-        // again...
-        void OnDestroy()
-        {
-            Destroy(backToLEButton);
-
-            LE_Object.ResetStaticVariablesInObjects();
-
-            PlaymodePauseMenuPatcher.DestroyPatcher();
-            UpgradePatches.Unpatch();
-        }
-
-        void TeleportPlayer()
-        {
-            LE_Player_Spawn spawn = FindObjectOfType<LE_Player_Spawn>();
-
-            if (!spawn)
-            {
-                Logger.Error("Couldn't find player spawn object in the level!");
-                LE_CustomErrorPopups.NoPlayerSpawnObjectDetected();
-                return;
-            }
-
-            Controls.Instance.transform.position = spawn.transform.position + Vector3.up;
-            Controls.Instance.gameCamera.transform.localPosition = new Vector3(0f, 0.907f, 0f);
-            Controls.Instance.gameCamera.transform.eulerAngles = spawn.transform.eulerAngles;
-            Controls.Instance.Angle = new Vector2(spawn.transform.eulerAngles.y, spawn.transform.eulerAngles.x);
-            Controls.Instance.transform.localScale = spawn.transform.localScale;
-        }
-
-        void DisableTheCurrentScene()
-        {
-            GameObject[] sceneObjects = SceneManager.GetActiveScene().GetRootGameObjects();
-
-            foreach (GameObject obj in sceneObjects)
-            {
-                if (obj.name == gameObject.name) continue;
-                if (obj.name == "Character") continue;
-                if (obj.name == "FootStepController") continue;
-                if (obj.name == "Checkpoints") continue;
-                if (obj.name == "LevelObjects") continue;
-                if (obj.name == "Player") continue;
-                if (obj.name == "GUI") continue;
-
-                obj.SetActive(false);
-            }
-        }
-
-        void ConfigureGlobalProperties()
-        {
-            if (!(bool)GetGlobalProperty("HasTaser"))
-            {
-                Controls.Instance.DeactivateWeapon();
-            }
-            Controls.Instance.hasJetPack = (bool)GetGlobalProperty("HasJetpack");
-
-            SetupLevelSkybox((int)GetGlobalProperty("Skybox"));
-
-            ApplyUpgrades((List<UpgradeSaveData>)GetGlobalProperty("Upgrades"));
-        }
-        object GetGlobalProperty(string name)
-        {
-            if (globalProperties.ContainsKey(name))
-            {
-                return globalProperties[name];
-            }
-
-            return null;
-        }
-
-        public GameObject PlaceObject(LE_Object.ObjectType? objectType, Vector3 position, Vector3 eulerAngles, Vector3 scale, bool setAsSelected = true)
-        {
-            if (objectType == null)
-            {
-                Logger.Error("objectType is null. Skipping object placement...");
-                return null;
-            }
-
-            GameObject template = allCategoriesObjects[objectType.Value];
-            GameObject obj = Instantiate(template, levelObjectsParent.transform);
-
-            obj.transform.localPosition = position;
-            obj.transform.localEulerAngles = eulerAngles;
-            obj.transform.localScale = scale;
-
-            LE_Object addedComp = LE_Object.AddComponentToObject(obj, objectType.Value);
-
-            if (objectType == LE_Object.ObjectType.SCREEN)
-            {
-                screensOnTheLevel.Add((LE_Screen)addedComp);
-            }
-            else if (objectType == LE_Object.ObjectType.SMALL_SCREEN)
-            {
-                smallScreensOnTheLevel.Add((LE_Small_Screen)addedComp);
-            }
-
-            if (addedComp == null)
-            {
-                Destroy(obj);
-                return null;
-            }
-
-            obj.SetActive(true);
-
-            return obj;
-        }
-
-        void ApplyUpgrades(List<UpgradeSaveData> upgrades)
-        {
-            foreach (var upgrade in upgrades)
-            {
-                switch (upgrade.type)
-                {
-                    case UpgradeType.DODGE:
-                        Controls.m_hasDodgeSkill = upgrade.active;
-                        Controls.m_currentDodgeLevel = upgrade.level;
-                        break;
-                }
-            }
-
-            // Not only appy the values to the player, but also just in case for the UI, etc.
-            UpgradePatches.Init();
-        }
-
-        void CreateBackToLEButton()
-        {
-            GameObject template = GameObject.Find("MainMenu/Camera/Holder/Main/LargeButtons/2_Chapters");
-            backToLEButton = Instantiate(template, template.transform.parent);
-            backToLEButton.name = "4_BackToLE";
-            Destroy(backToLEButton.GetComponent<ButtonController>());
-            Destroy(backToLEButton.GetChildWithName("Label").GetComponent<UILocalize>());
-            backToLEButton.GetChildWithName("Label").GetComponent<UILabel>().text = "Back to Level Editor";
-
-            backToLEButton.GetComponent<UIButton>().onClick.Add(new EventDelegate(this, nameof(GoBackToLEWhileInPlayMode)));
-
-            backToLEButton.SetActive(true);
-        }
-        void GoBackToLEWhileInPlayMode()
-        {
-            Invoke("DestroyBackToLEButton", 0.2f);
-            LE_MenuUIManager.Instance.GoBackToLEWhileInPlayMode(levelFileNameWithoutExtension, levelName);
-        }
-        void DestroyBackToLEButton()
-        {
-            Destroy(backToLEButton);
         }
 
         void LoadAssetBundle()
@@ -255,7 +98,136 @@ namespace FS_LevelEditor.Playmode
         {
             return otherObjectsFromBundle.FirstOrDefault(obj => obj.name == objectName);
         }
+        public void UnloadBundle()
+        {
+            LEBundle.Unload(false);
+        }
 
+        void Start()
+        {
+            TeleportPlayer();
+            ConfigureGlobalProperties();
+
+            UnloadBundle();
+        }
+
+        void CreateBackToLEButton()
+        {
+            GameObject template = GameObject.Find("MainMenu/Camera/Holder/Main/LargeButtons/2_Chapters");
+            backToLEButton = Instantiate(template, template.transform.parent);
+            backToLEButton.name = "4_BackToLE";
+            Destroy(backToLEButton.GetComponent<ButtonController>());
+            Destroy(backToLEButton.GetChildWithName("Label").GetComponent<UILocalize>());
+            backToLEButton.GetChildWithName("Label").GetComponent<UILabel>().text = "Back to Level Editor";
+
+            backToLEButton.GetComponent<UIButton>().onClick.Add(new EventDelegate(this, nameof(GoBackToLEWhileInPlayMode)));
+
+            backToLEButton.SetActive(true);
+        }
+        void GoBackToLEWhileInPlayMode()
+        {
+            Invoke("DestroyBackToLEButton", 0.2f);
+            LE_MenuUIManager.Instance.GoBackToLEWhileInPlayMode(levelFileNameWithoutExtension, levelName);
+        }
+        void DestroyBackToLEButton()
+        {
+            Destroy(backToLEButton);
+        }
+
+        void DisableTheCurrentScene()
+        {
+            GameObject[] sceneObjects = SceneManager.GetActiveScene().GetRootGameObjects();
+
+            foreach (GameObject obj in sceneObjects)
+            {
+                if (obj.name == gameObject.name) continue;
+                if (obj.name == "Character") continue;
+                if (obj.name == "FootStepController") continue;
+                if (obj.name == "Checkpoints") continue;
+                if (obj.name == "LevelObjects") continue;
+                if (obj.name == "Player") continue;
+                if (obj.name == "GUI") continue;
+
+                obj.SetActive(false);
+            }
+        }
+        void TeleportPlayer()
+        {
+            LE_Player_Spawn spawn = FindObjectOfType<LE_Player_Spawn>();
+
+            if (!spawn)
+            {
+                Logger.Error("Couldn't find player spawn object in the level!");
+                LE_CustomErrorPopups.NoPlayerSpawnObjectDetected();
+                return;
+            }
+
+            Controls.Instance.transform.position = spawn.transform.position + Vector3.up;
+            Controls.Instance.gameCamera.transform.localPosition = new Vector3(0f, 0.907f, 0f);
+            Controls.Instance.gameCamera.transform.eulerAngles = spawn.transform.eulerAngles;
+            Controls.Instance.Angle = new Vector2(spawn.transform.eulerAngles.y, spawn.transform.eulerAngles.x);
+            Controls.Instance.transform.localScale = spawn.transform.localScale;
+        }
+
+        public GameObject PlaceObject(LE_Object.ObjectType? objectType, Vector3 position, Vector3 eulerAngles, Vector3 scale, bool setAsSelected = true)
+        {
+            if (objectType == null)
+            {
+                Logger.Error("objectType is null. Skipping object placement...");
+                return null;
+            }
+
+            GameObject template = allCategoriesObjects[objectType.Value];
+            GameObject obj = Instantiate(template, levelObjectsParent.transform);
+
+            obj.transform.localPosition = position;
+            obj.transform.localEulerAngles = eulerAngles;
+            obj.transform.localScale = scale;
+
+            LE_Object addedComp = LE_Object.AddComponentToObject(obj, objectType.Value);
+
+            if (objectType == LE_Object.ObjectType.SCREEN)
+            {
+                screensOnTheLevel.Add((LE_Screen)addedComp);
+            }
+            else if (objectType == LE_Object.ObjectType.SMALL_SCREEN)
+            {
+                smallScreensOnTheLevel.Add((LE_Small_Screen)addedComp);
+            }
+
+            if (addedComp == null)
+            {
+                Destroy(obj);
+                return null;
+            }
+
+            obj.SetActive(true);
+
+            return obj;
+        }
+
+        void ConfigureGlobalProperties()
+        {
+            if (!(bool)GetGlobalProperty("HasTaser"))
+            {
+                Controls.Instance.DeactivateWeapon();
+            }
+            Controls.Instance.hasJetPack = (bool)GetGlobalProperty("HasJetpack");
+
+            SetupLevelSkybox((int)GetGlobalProperty("Skybox"));
+
+            ApplyUpgrades((List<UpgradeSaveData>)GetGlobalProperty("Upgrades"));
+        }
+        object GetGlobalProperty(string name)
+        {
+            if (globalProperties.ContainsKey(name))
+            {
+                return globalProperties[name];
+            }
+
+            return null;
+        }
+        // --------------------------------------------------
         void SetupLevelSkybox(int skyboxID)
         {
             string skyboxMatName = $"Skybox_CH{skyboxID + 1}";
@@ -264,12 +236,24 @@ namespace FS_LevelEditor.Playmode
             skyboxMat.shader = Shader.Find("Skybox/6 Sided 3 Axis Rotation");
             RenderSettings.skybox = skyboxMat;
         }
-
-        public void UnloadBundle()
+        void ApplyUpgrades(List<UpgradeSaveData> upgrades)
         {
-            LEBundle.Unload(false);
+            foreach (var upgrade in upgrades)
+            {
+                switch (upgrade.type)
+                {
+                    case UpgradeType.DODGE:
+                        Controls.m_hasDodgeSkill = upgrade.active;
+                        Controls.m_currentDodgeLevel = upgrade.level;
+                        break;
+                }
+            }
+
+            // Not only appy the values to the player, but also just in case for the UI, etc.
+            UpgradePatches.Init();
         }
 
+        // Other stuff...
         public void PatchPauseCurrentLevelNameInResumeButton()
         {
             MelonCoroutines.Start(Coroutine());
@@ -279,7 +263,6 @@ namespace FS_LevelEditor.Playmode
                 MenuController.GetInstance().levelToResumeLabel.text = "Custom Level : " + levelName;
             }
         }
-
         public void InvertPlayerGravity()
         {
             Controls.Instance.InverseGravity();
@@ -296,6 +279,18 @@ namespace FS_LevelEditor.Playmode
 
                 screen.TriggerAction("InvertText");
             }
+        }
+
+        void OnDestroy()
+        {
+            // When the script obj is destroyed, that means the scene has changed, destroy the back to LE button, since it'll be created again when entering...
+            // again...
+            Destroy(backToLEButton);
+
+            LE_Object.ResetStaticVariablesInObjects();
+
+            PlaymodePauseMenuPatcher.DestroyPatcher();
+            UpgradePatches.Unpatch();
         }
     }
 }
