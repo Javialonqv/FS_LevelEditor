30cab663a41078d4149f47ec79d9aefe8ba4bcae
Javier Quintero <alonsoquinterovizcarra@gmail.com>
2025-11-20 18:52:32 -0700

    Fixed the "heart beat" bug, where that sound was still playing even after healing the player with a heal area.

--- LE_Objects/LE_Heal_Area.cs
+++ LE_Objects/LE_Heal_Area.cs
@@ -6,12 +6,18 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Events;
+using HarmonyLib;
+using MelonLoader;
+using System.Collections;
 
 namespace FS_LevelEditor
 {
     [MelonLoader.RegisterTypeInIl2Cpp]
     public class LE_Heal_Area : LE_Object
     {
+        HealArea script;
+        public Coroutine smallHealPatchCoroutine;
+
         void Awake()
         {
             properties = new Dictionary<string, object>
@@ -38,7 +44,7 @@ namespace FS_LevelEditor
             areaObj.tag = "HealArea";
             areaObj.layer = LayerMask.NameToLayer("Ignore Raycast");
 
-            HealArea script = areaObj.AddComponent<HealArea>();
+            script = areaObj.AddComponent<HealArea>();
             script.halfStatusObj = new GameObject("ShouldBeSaved");
             script.emptyStatusObj = new GameObject("ShouldBeSaved");
             script.vfx = new GameObject("ShouldBeSaved").AddComponent<ParticleSystem>();
@@ -103,9 +109,64 @@ namespace FS_LevelEditor
             return base.SetProperty(name, value);
         }
 
+        // This is a coroutine that will be (hopefully) executed along with the HealRoutine in the original HealArea script.
+        public IEnumerator SmallHealPatchRoutine()
+        {
+            // The Controls class usually uses the HealCharacter function in order to cure the player, and also calls OnHasNormalHealth,
+            // which stops the heart beat.
+            // However, since that function seems to heal the WHOLE player, HealArea uses another aproach.
+            // In older mono versions of FS, it seems that HealArea calls a SmallHeal function inside of Controls, which does NOT call
+            // OnHasNormalHealth, therefore, the game never stops the hearth beats, so we need to do the check and call it ourselves.
+
+            while (true)
+            {
+                if (Controls.Instance.currentHP > Controls.Instance.m_lowHealthThreshold)
+                {
+                    Controls.Instance.StopHeartBeatSound();
+                }
+                yield return new WaitForSeconds(script.healInterval);
+            }
+        }
+
         public static new Color GetDefaultObjectColor(LEObjectContext context)
         {
             return new Color(0f, 1f, 0.65098039215686274509803921568627f);
+        }
+    }
+
+    [HarmonyLib.HarmonyPatch(typeof(HealArea), nameof(HealArea.StartHeal))]
+    public static class SmallHealPatch
+    {
+        public static void Postfix(HealArea __instance)
+        {
+            // Check if it has the LE component of HealArea.
+            if (__instance.transform.parent && __instance.transform.parent.parent &&
+                __instance.transform.parent.parent.TryGetComponent(out LE_Heal_Area leScript))
+            {
+                if (leScript.smallHealPatchCoroutine == null)
+                {
+                    Logger.Log($"A heal area \"{leScript.objectFullNameWithID}\" was reached! Patching small heal bug...");
+                    leScript.smallHealPatchCoroutine = (Coroutine)MelonCoroutines.Start(leScript.SmallHealPatchRoutine());
+                }
+            }
+        }
+    }
+    [HarmonyLib.HarmonyPatch(typeof(HealArea), nameof(HealArea.StopHeal))]
+    public static class SmallHealUnPatch
+    {
+        public static void Postfix(HealArea __instance)
+        {
+            // Check if it has the LE component of HealArea.
+            if (__instance.transform.parent && __instance.transform.parent.parent &&
+                __instance.transform.parent.parent.TryGetComponent(out LE_Heal_Area leScript))
+            {
+                if (leScript.smallHealPatchCoroutine != null)
+                {
+                    Logger.Log($"A heal area \"{leScript.objectFullNameWithID}\" stopped healing! Undoing small heal patch...");
+                    MelonCoroutines.Stop(leScript.smallHealPatchCoroutine);
+                    leScript.smallHealPatchCoroutine = null;
+                }
+            }
         }
     }
 }
