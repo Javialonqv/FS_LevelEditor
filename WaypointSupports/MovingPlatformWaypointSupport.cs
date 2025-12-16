9e6713f4cc5f194d00df2aaff7a22a30ba1becfa
Javier Quintero <alonsoquinterovizcarra@gmail.com>
2025-12-15 17:56:15 -0700

    Fixed MovingPlatformWaypointSupport.cs formatting.

--- WaypointSupports/MovingPlatformWaypointSupport.cs
+++ WaypointSupports/MovingPlatformWaypointSupport.cs
@@ -13,35 +13,35 @@ namespace FS_LevelEditor.WaypointSupports
     public class MovingPlatformWaypointSupport : WaypointSupport
     {
         public override List<WaypointData> targetWaypointsData => targetObject.GetProperty<List<WaypointData>>("waypoints");
-      public override LE_Object.ObjectType waypointTypeToUse => LE_Object.ObjectType.MOVING_PLATFORM_WAYPOINT;
+        public override LE_Object.ObjectType waypointTypeToUse => LE_Object.ObjectType.MOVING_PLATFORM_WAYPOINT;
         public override bool needsEmptyWaypointAtStart => true;
-      public override bool usesCustomMoveSystem => true;
-     public override Color editorLineColor => Color.yellow;
+        public override bool usesCustomMoveSystem => true;
+        public override Color editorLineColor => Color.yellow;
         public override GameObject waypointTemplate => Core.LoadOtherObjectInBundle("Moving Platform Waypoint");
 
- public override void SetupForCustomSystem()
+        public override void SetupForCustomSystem()
         {
- MovingPlatformController platformScript = gameObject.GetChild("Content").GetComponent<MovingPlatformController>();
+            MovingPlatformController platformScript = gameObject.GetChild("Content").GetComponent<MovingPlatformController>();
 
-   // Make the first waypoint match with the start position of the platform.
-      spawnedWaypoints[0].transform.localPosition = new Vector3(0, 0.135f, 0);
+            // Make the first waypoint match with the start position of the platform.
+            spawnedWaypoints[0].transform.localPosition = new Vector3(0, 0.135f, 0);
 
-   platformScript.currentWaypoint = spawnedWaypoints[0].gameObject;
-   platformScript.currentWaypointScript = spawnedWaypoints[0].GetComponent<Waypoint>();
+            platformScript.currentWaypoint = spawnedWaypoints[0].gameObject;
+            platformScript.currentWaypointScript = spawnedWaypoints[0].GetComponent<Waypoint>();
 
-   // CRITICAL FIX: Add rotation applier that will rotate the platform to match waypoint rotation
-    // AFTER reaching each waypoint, not during transit. This prevents waypoint rotation from
-         // affecting the platform's heading while moving.
-         var rotationApplier = gameObject.AddComponent<WaypointRotationApplier>();
- rotationApplier.targetTransform = gameObject.GetChild("Content").transform;
-    rotationApplier.waypointSupport = this;
+            // CRITICAL FIX: Add rotation applier that will rotate the platform to match waypoint rotation
+            // AFTER reaching each waypoint, not during transit. This prevents waypoint rotation from
+            // affecting the platform's heading while moving.
+            var rotationApplier = gameObject.AddComponent<WaypointRotationApplier>();
+            rotationApplier.targetTransform = gameObject.GetChild("Content").transform;
+            rotationApplier.waypointSupport = this;
         }
 
         public override WaypointMode GetWaypointMode()
         {
-   LE_Moving_Platform platform = GetComponent<LE_Moving_Platform>();
+            LE_Moving_Platform platform = GetComponent<LE_Moving_Platform>();
 
-          return platform.GetProperty<WaypointMode>("MovementMode");
+            return platform.GetProperty<WaypointMode>("MovementMode");
         }
     }
 }
