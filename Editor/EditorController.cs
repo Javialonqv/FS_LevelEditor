9169d8b91709a0eb657855d40d3e93bf9804da0e
Javier Quintero <alonsoquinterovizcarra@gmail.com>
2026-01-22 17:44:47 -0700

    Fixed a bug where duplicating objects broke their waypoints (if they had any) and reset their position.

--- Editor/EditorController.cs
+++ Editor/EditorController.cs
@@ -2674,6 +2674,7 @@ namespace FS_LevelEditor.Editor
                 {
                     newPlacedObjComp.SetProperty(property.Key, Utils.CreateCopyOf(property.Value));
                 }
+                objComponent.BeforeSave(); // If the origin obj was waypoints, force them to update their position, rotation and props before copying them.
                 foreach (var waypoint in objComponent.waypoints)
                 {
                     newPlacedObjComp.waypoints.Add((WaypointData)Utils.CreateCopyOf(waypoint));
