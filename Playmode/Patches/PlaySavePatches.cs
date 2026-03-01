1d711f3a32e2c72147a458fc04f5786de125ccb0
Javier Quintero <alonsoquinterovizcarra@gmail.com>
2026-03-01 16:34:29 -0700

    Now saving FS settings while in playmode should work + THE FUCKING CURRENT CHAPTER IS NOT OVERRIDED BY LE ANYMORE.

--- Playmode/Patches/PlaySavePatches.cs
+++ Playmode/Patches/PlaySavePatches.cs
@@ -1,39 +1,47 @@
-﻿using Il2Cpp;
+﻿using FS_LevelEditor.SaveSystem;
+using HarmonyLib;
+using Il2Cpp;
+using Il2CppInControl;
 using MelonLoader;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using HarmonyLib;
-using FS_LevelEditor.SaveSystem;
 
 namespace FS_LevelEditor.Playmode.Patches
 {
-    [HarmonyPatch(typeof(FractalSave), nameof(FractalSave.SaveKey), [typeof(string), typeof(int), typeof(bool), typeof(bool)])]
+    [HarmonyPatch(typeof(FractalSave), nameof(FractalSave.SaveKey), [typeof(string), typeof(string), typeof(bool), typeof(bool)])]
     public static class SaveKeyIntPatch
     {
         public static bool Prefix(string _key)
         {
-            if (PlayModeController.Instance)
+            // Don't save when the user is ending the level in playmode.
+            if (PlayModeController.Instance && PlayModeController.Instance.endTriggerReached)
             {
-                // Don't save when the user is ending the level in playmode.
-                if (PlayModeController.Instance.endTriggerReached)
+                // Except when it's saving time.
+                // ??? This thing is not even being used, explain GrAI.
+                if (_key.EndsWith("_LETime"))
                 {
-                    // Allow saving time even when ending level
-                    if (_key.EndsWith("_LETime"))
-                    {
-                        return true;
-                    }
+                    return true;
                 }
 
                 return false;
             }
 
+            // Don't save the current level when you're loading playmode (which will be Chapter 4).
+            if (PlayModeController.Instance || Melon<Core>.Instance.loadCustomLevelOnSceneLoad)
+            {
+                if (_key == "Current_Level" || _key == "Last_Checkpoint")
+                {
+                    return false;
+                }
+            }
+
             return true;
         }
     }
-    [HarmonyPatch(typeof(FractalSave), nameof(FractalSave.SaveLevelKey), [typeof(string), typeof(int), typeof(bool), typeof(bool)])]
+    [HarmonyPatch(typeof(FractalSave), nameof(FractalSave.SaveLevelKey), [typeof(string), typeof(string), typeof(bool), typeof(bool)])]
     public static class SaveLevelKeyIntPatch
     {
         public static bool Prefix(string _key)
@@ -47,10 +55,38 @@ namespace FS_LevelEditor.Playmode.Patches
             // Don't save the current level when you're loading playmode (which will be Chapter 4).
             if (PlayModeController.Instance || Melon<Core>.Instance.loadCustomLevelOnSceneLoad)
             {
-                if (_key == "Current_Level")
+                if (_key == "Current_Level" || _key == "Last_Checkpoint")
                 {
                     return false;
                 }
+            }
+
+            return true;
+        }
+    }
+    [HarmonyPatch(typeof(FractalSave), nameof(FractalSave.DeleteQuickSaveSaveForAllLevels))]
+    public static class DeleteQuickSaveFilesPatch
+    {
+        public static bool Prefix()
+        {
+            // Don't delete quick save files when about to play an LE level.
+            if (PlayModeController.Instance || Melon<Core>.Instance.loadCustomLevelOnSceneLoad)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+    [HarmonyPatch(typeof(FractalSave), nameof(FractalSave.DeleteAutoSaveSaveForAllLevels))]
+    public static class DeleteAutoSaveFilesPatch
+    {
+        public static bool Prefix()
+        {
+            // Don't delete auto save files when about to play an LE level.
+            if (PlayModeController.Instance || Melon<Core>.Instance.loadCustomLevelOnSceneLoad)
+            {
+                return false;
             }
 
             return true;
