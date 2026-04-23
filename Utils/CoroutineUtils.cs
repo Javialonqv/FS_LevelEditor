5f9aaf3c2caceaed60bfd30ffe783a12c0974f27
Javier Quintero <alonsoquinterovizcarra@gmail.com>
2026-04-22 17:01:26 -0700

    Added CoroutineUtils.cs

---
+++ Utils/CoroutineUtils.cs
@@ -0,0 +1,55 @@
+﻿using MelonLoader;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FS_LevelEditor
+{
+    public static class CoroutineUtils
+    {
+        static Dictionary<string, List<object>> executingCoroutinesWithIDs = new();
+
+        public static void Start(IEnumerator coroutine)
+        {
+            MelonCoroutines.Start(coroutine);
+        }
+        public static void Start(IEnumerator coroutine, string id)
+        {
+            if (!executingCoroutinesWithIDs.ContainsKey(id))
+                executingCoroutinesWithIDs.Add(id, new());
+
+            object coroutineToken = MelonCoroutines.Start(coroutine);
+            executingCoroutinesWithIDs[id].Add(coroutineToken);
+        }
+
+        public static void Stop(object coroutineToken)
+        {
+            foreach (var keyPair in executingCoroutinesWithIDs)
+            {
+                if (keyPair.Value.Remove(coroutineToken))
+                {
+                    break;
+                }
+            }
+
+            MelonCoroutines.Stop(coroutineToken);
+        }
+        public static void StopAllCoroutines(string coroutinesID)
+        {
+            if (!executingCoroutinesWithIDs.ContainsKey(coroutinesID))
+                return;
+
+            foreach (var coroutine in executingCoroutinesWithIDs[coroutinesID])
+            {
+                if (coroutine != null)
+                    MelonCoroutines.Stop(coroutine);
+            }
+
+            executingCoroutinesWithIDs.Remove(coroutinesID);
+        }
+    }
+}
