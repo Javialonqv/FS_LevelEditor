165fcc8199f7dfada7b9ec7633346a4df3349983
Javier Quintero <alonsoquinterovizcarra@gmail.com>
2026-07-15 20:41:37 -0700

    Added Light and Balls options for the XMAS Tree.

--- LE_Objects/LE_Xmas_Tree.cs
+++ LE_Objects/LE_Xmas_Tree.cs
@@ -12,23 +12,31 @@ namespace FS_LevelEditor
     public class LE_Xmas_Tree : LE_Object
     {
         GameObject presentsParent;
+        GameObject light;
+        GameObject ballsParent;
 
         public static Dictionary<string, object> GetDefaultProperties()
         {
             return new Dictionary<string, object>
             {
-                { "Presents", true }
+                { "Presents", true },
+                { "Light", true },
+                { "Balls", true }
             };
         }
 
         void Awake()
         {
             presentsParent = contentObject.GetChild("PresentsPack");
+            light = contentObject.GetChild("Garland_lights");
+            ballsParent = contentObject.GetChild("Balls");
         }
 
         public override void ObjectStart(LEScene scene)
         {
             SetPresentsState(GetProperty<bool>("Presents"));
+            SetLightState(GetProperty<bool>("Light"));
+            SetBallsState(GetProperty<bool>("Balls"));
 
             base.ObjectStart(scene);
         }
@@ -45,6 +53,26 @@ namespace FS_LevelEditor
                     return true;
                 }
             }
+            else if (name == "Light")
+            {
+                if (value is bool boolValue)
+                {
+                    properties["Light"] = boolValue;
+                    if (EditorController.Instance)
+                        SetLightState(boolValue);
+                    return true;
+                }
+            }
+            else if (name == "Balls")
+            {
+                if (value is bool boolValue)
+                {
+                    properties["Balls"] = boolValue;
+                    if (EditorController.Instance)
+                        SetBallsState(boolValue);
+                    return true;
+                }
+            }
 
             return base.SetProperty(name, value);
         }
@@ -52,6 +80,35 @@ namespace FS_LevelEditor
         void SetPresentsState(bool enabled)
         {
             presentsParent.SetActive(enabled);
+            if (EditorController.Instance)
+            {
+                foreach (var waypoint in waypointSupport.spawnedWaypoints)
+                {
+                    waypoint.contentObject.GetChild("PresentsPack").SetActive(enabled);
+                }
+            }
+        }
+        void SetLightState(bool enabled)
+        {
+            light.SetActive(enabled);
+            if (EditorController.Instance)
+            {
+                foreach (var waypoint in waypointSupport.spawnedWaypoints)
+                {
+                    waypoint.contentObject.GetChild("Garland_lights").SetActive(enabled);
+                }
+            }
+        }
+        void SetBallsState(bool enabled)
+        {
+            ballsParent.SetActive(enabled);
+            if (EditorController.Instance)
+            {
+                foreach (var waypoint in waypointSupport.spawnedWaypoints)
+                {
+                    waypoint.contentObject.GetChild("Balls").SetActive(enabled);
+                }
+            }
         }
     }
 }
