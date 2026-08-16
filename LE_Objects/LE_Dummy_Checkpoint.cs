469f3bc61c9c095e5251897a57f4da544359376b
Javier Quintero <alonsoquinterovizcarra@gmail.com>
2026-08-16 12:40:53 -0700

    Fixed not being able to drop cubes after respawning in a checkpoint while you're still grabbing them.

--- LE_Objects/LE_Dummy_Checkpoint.cs
+++ LE_Objects/LE_Dummy_Checkpoint.cs
@@ -25,6 +25,7 @@ namespace FS_LevelEditor
 
         static bool HasGun;
         static bool HasJetpack;
+        static BlocScript ActiveBloc;
 
         public override void InitComponent()
         {
@@ -99,7 +100,7 @@ namespace FS_LevelEditor
             AtLeastOneCheckpointReached = true;
             LastReachedCheckpoint = this;
 
-            UpdateHasGunAndJetpackValues();
+            UpdateStaticValues();
 
             eventExecuter.ExecuteEventsWithAndLogic((List<LE_Event>)properties["OnSave"], "OnSave", true);
         }
@@ -115,14 +116,19 @@ namespace FS_LevelEditor
             Controls.Instance.hasJetPack = HasJetpack;
             Controls.Instance.jetPackObject.SetActive(HasJetpack);
 
+            if (ActiveBloc)
+                ActivableController.activeCubeForInteraction = ActiveBloc;
+
             eventExecuter.ExecuteEventsWithAndLogic((List<LE_Event>)properties["OnRespawn"], "OnRespawn", true);
         }
 
-        public static void UpdateHasGunAndJetpackValues()
+        public static void UpdateStaticValues()
         {
             // NOTE: They're static, it's intended that the player keeps the taser/jetpack even if he didn't have it when he reached the checkpoint (as long as he acquired them lol).
             HasGun = Controls.Instance.HasTaser();
             HasJetpack = Controls.Instance.hasJetPack;
+
+            ActiveBloc = ActivableController.activeCubeForInteraction;
         }
 
         // FS uses SavedObjeTsHolder.AllCheckpoints to find the current checkpoint (because it's not cached for some reason), and then read the variables from it.
