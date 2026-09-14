e10d1ede8d2a6384d9f1d821f129a8eb615ba581
Javier Quintero <alonsoquinterovizcarra@gmail.com>
2026-09-14 12:23:38 -0700

    Thanks for telling me Cafe!

--- Core.cs
+++ Core.cs
@@ -14,7 +14,7 @@ namespace FS_LevelEditor
         // MOD METADATA
         public const string ModName = "Level Editor";
         public const string Author = "Javialon_qv & Cafe";
-        public const string Version = "1.0.0";
+        public const string Version = "0.1.0";
         public const string Description = "An unofficial Level Editor made for Fractal Space!";
         public const bool SupportsHotReload = false;
 
