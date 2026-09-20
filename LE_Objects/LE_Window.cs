33f20b71ea772cc0d2412cf260c538efd80af4e9
whitesharkcafe <305790387+whitesharkcafee@users.noreply.github.com>
2026-09-20 04:18:02 -0700

    Added border property to Window

---
+++ LE_Objects/LE_Window.cs
@@ -0,0 +1,60 @@
+﻿using FS_LevelEditor.Editor;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+namespace FS_LevelEditor
+{ 
+    public class LE_Window : LE_Object
+    {
+        GameObject border;
+        void Awake()
+        {
+            border = gameObject.GetChildAt("Content/Border");
+        }
+        public static Dictionary<string, object> GetDefaultProperties()
+        {
+            return new Dictionary<string, object>()
+            {
+                { "Border", true },
+            };
+        }
+        public override void ObjectStart(LEScene scene)
+        {
+            if (scene == LEScene.Editor)
+            {
+                UpdateBorderState(GetProperty<bool>("Border"));
+            }
+
+            base.ObjectStart(scene);
+        }
+        public override void InitComponent()
+        {
+
+            UpdateBorderState(GetProperty<bool>("Border"));
+
+            base.InitComponent();
+        }
+
+        public override bool SetProperty(string name, object value)
+        {
+            if (name == "Border")
+            {
+                if (value is bool boolValue)
+                {
+                    properties["Border"] = boolValue;
+                    if (EditorController.Instance) UpdateBorderState(boolValue);
+                }
+            }
+
+            return base.SetProperty(name, value);
+        }
+        void UpdateBorderState(bool enabled)
+        {
+            border.GetComponent<MeshRenderer>().enabled = enabled;
+            foreach (var waypoint in waypointSupport.spawnedWaypoints)
+            {
+                waypoint.gameObject.GetChildAt("Content/Border").GetComponent<MeshRenderer>().enabled = enabled;
+            }
+        }
+    }
+}
